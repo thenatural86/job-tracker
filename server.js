0bc254d01455d0b1330c98cd104b6cd596015a57
thenatural86 <philip.smalls86@gmail.com>
2024-05-08 10:55:28 -0300

    adds middleware

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from 'express'
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/', (req, res) => {
+  console.log(req)
+
+  res.json({ message: 'data recieved', data: req.body })
 })
 
 app.listen(5100, () => {
