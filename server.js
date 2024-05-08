5a9a36e16a07acdf2c9a77bbdcc6b37308127e86
thenatural86 <philip.smalls86@gmail.com>
2024-05-08 16:47:56 -0300

    get all jobs route

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import express from 'express'
 const app = express()
 
 import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+
+  { id: nanoid(), company: 'google', position: 'back-end' },
+]
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -19,6 +26,10 @@ app.post('/', (req, res) => {
   console.log(req)
 
   res.json({ message: 'data recieved', data: req.body })
+})
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs })
 })
 
 const port = process.env.PORT || 5100
