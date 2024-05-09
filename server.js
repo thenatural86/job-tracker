77d552f43ffddf691e2530a229b53650f24ec1da
thenatural86 <philip.smalls86@gmail.com>
2024-05-09 15:19:46 -0300

    adds routes and controller folders

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ import express from 'express'
 const app = express()
 
 import morgan from 'morgan'
-import { nanoid } from 'nanoid'
 
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end' },
-
-  { id: nanoid(), company: 'google', position: 'back-end' },
-]
+// routes
+import jobRouter from './routes/jobRouter.js'
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -24,72 +20,10 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log(req)
-
   res.json({ message: 'data recieved', data: req.body })
 })
 
-// Get All Jobs
-app.get('/api/v1/jobs', (req, res) => {
-  res.status(200).json({ jobs })
-})
-
-// Create Job
-app.post('/api/v1/jobs', (req, res) => {
-  const { company, position } = req.body
-
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' })
-  }
-
-  const id = nanoid(10)
-  const job = { id, company, position }
-  jobs = [...jobs, job]
-  res.status(200).json({ job })
-})
-
-// Get Single Job
-app.get('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params
-  const job = jobs.find((job) => job.id === id)
-
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-  res.status(200).json({ job })
-})
-
-// Edit Job
-app.patch('/api/v1/jobs/:id', (req, res) => {
-  const { company, position } = req.body
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' })
-  }
-
-  const { id } = req.params
-  const job = jobs.find((job) => job.id === id)
-
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-
-  job.company = company
-  job.position = position
-  res.status(200).json({ msg: 'job modified', job })
-})
-
-// Delete Job
-app.delete('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params
-  const job = jobs.find((job) => job.id === id)
-
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-
-  const newJobs = jobs.filter((job) => job.id !== id)
-  jobs = newJobs
-  res.status(200).json({ msg: 'job deleted' })
-})
+app.use('/api/v1/jobs', jobRouter)
 
 // Not Found Middleware
 app.use('*', (req, res) => {
