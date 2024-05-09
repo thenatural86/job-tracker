c5b44936d48668ab7e012e946ffe44d9548af825
thenatural86 <philip.smalls86@gmail.com>
2024-05-09 14:38:46 -0300

    adds error and not found middleware

--- server.js
+++ server.js
@@ -91,6 +91,17 @@ app.delete('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ msg: 'job deleted' })
 })
 
+// Not Found Middleware
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' })
+})
+
+// Error Middleware
+app.use((err, req, res, next) => {
+  console.log(error)
+  res.status(500).json({ msg: 'something went wrong' })
+})
+
 const port = process.env.PORT || 5100
 
 app.listen(port, () => {
