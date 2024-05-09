1b6e27ae9e2226254f432778bb4c8e415169fd17
thenatural86 <philip.smalls86@gmail.com>
2024-05-09 10:08:26 -0300

    edit job route

--- server.js
+++ server.js
@@ -47,6 +47,41 @@ app.post('/api/v1/jobs', (req, res) => {
   res.status(200).json({ job })
 })
 
+// Get Single Job
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params
+  const job = jobs.find((job) => job.id === id)
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` })
+  }
+  res.status(200).json({ job })
+})
+
+// Edit Job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' })
+  }
+
+  const { id } = req.params
+  const job = jobs.find((job) => job.id === id)
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` })
+  }
+
+  job.company = company
+  job.position = position
+  res.status(200).json({ msg: 'job modified', job })
+})
+
+// Delete Job
+app.post('/api/v1/jobs/:id', (req, res) => {
+  res.send('delete job')
+})
+
 const port = process.env.PORT || 5100
 
 app.listen(port, () => {
