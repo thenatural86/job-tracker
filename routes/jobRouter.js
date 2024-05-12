2f0f6b79bef1668eb82b7f57e298cf6050f8f71c
thenatural86 <philip.smalls86@gmail.com>
2024-05-12 20:02:18 -0300

    adds job validators

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -9,10 +9,16 @@ import {
   deleteJob,
 } from '../controllers/jobController.js'
 
+import { validateJobInput } from '../middleware/validationMiddleware.js'
+
 // router.get('/', getAllJobs)
 // router.post('/', createJob)
 
-router.route('/').get(getAllJobs).post(createJob)
-router.route('/:id').get(getSingleJob).patch(updateJob).delete(deleteJob)
+router.route('/').get(getAllJobs).post(validateJobInput, createJob)
+router
+  .route('/:id')
+  .get(getSingleJob)
+  .patch(validateJobInput, updateJob)
+  .delete(deleteJob)
 
 export default router
