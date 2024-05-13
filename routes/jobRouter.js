e956f44048627caf8b59a50cc6543825be52d80a
thenatural86 <philip.smalls86@gmail.com>
2024-05-13 10:53:35 -0300

    adds validation for id params

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -9,7 +9,10 @@ import {
   deleteJob,
 } from '../controllers/jobController.js'
 
-import { validateJobInput } from '../middleware/validationMiddleware.js'
+import {
+  validateJobInput,
+  validateIdParam,
+} from '../middleware/validationMiddleware.js'
 
 // router.get('/', getAllJobs)
 // router.post('/', createJob)
@@ -17,8 +20,8 @@ import { validateJobInput } from '../middleware/validationMiddleware.js'
 router.route('/').get(getAllJobs).post(validateJobInput, createJob)
 router
   .route('/:id')
-  .get(getSingleJob)
+  .get(validateIdParam, validateIdParam, getSingleJob)
   .patch(validateJobInput, updateJob)
-  .delete(deleteJob)
+  .delete(validateIdParam, deleteJob)
 
 export default router
