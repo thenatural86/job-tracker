06e888b28c27d497e723bf543417fb212fb1e9f3
thenatural86 <philip.smalls86@gmail.com>
2024-05-13 11:19:47 -0300

    adds validators to job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,5 @@
 import Job from '../models/JobModel.js'
 import { StatusCodes } from 'http-status-codes'
-import { NotFoundError } from '../errors/customErrors.js'
 
 // Get All Jobs
 export const getAllJobs = async (req, res) => {
@@ -16,35 +15,20 @@ export const createJob = async (req, res) => {
 
 // Get Single Job
 export const getSingleJob = async (req, res) => {
-  const { id } = req.params
-  const job = await Job.findById(id)
-
-  if (!job) throw new NotFoundError(`no job with id : ${id}`)
+  const job = await Job.findById(req.params.id)
   res.status(StatusCodes.OK).json({ job })
 }
 
 // Edit Job
 export const updateJob = async (req, res) => {
-  const { id } = req.params
-  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+  const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
-
-  if (!updatedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-
   res.status(StatusCodes.OK).json({ job: updatedJob })
 }
 
 // Delete Job
 export const deleteJob = async (req, res) => {
-  const { id } = req.params
-  const removedJob = await Job.findByIdAndDelete(id)
-
-  if (!removedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` })
-  }
-
+  const removedJob = await Job.findByIdAndDelete(req.params.id)
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob })
 }
