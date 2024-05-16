e67caaf2a3b17ed03b7db9ae44f89fc9d6a81c5c
thenatural86 <philip.smalls86@gmail.com>
2024-05-16 11:41:39 -0300

    user routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import { login } from '../controllers/authController.js'
 import { UnauthenticatedError } from '../errors/customErrors.js'
 import { verifyJWT } from '../utils/tokenUtils.js'
 
-export const authenticateUser = async (req, res, next) => {
+export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies
   if (!token) {
     throw new UnauthenticatedError('authentication invalid')
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from 'cookie-parser'
 // routes
 import jobRouter from './routes/jobRouter.js'
 import authRouter from './routes/authRouter.js'
+import userRouter from './routes/userRouter.js'
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMIddleware.js'
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter)
+app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
 // Not Found Middleware
