d1a092b769c9c787a71f47ac2b84a3912fbc3365
thenatural86 <philip.smalls86@gmail.com>
2024-05-15 22:24:13 -0300

    adds user to job routes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,12 +4,13 @@ import { StatusCodes } from 'http-status-codes'
 // Get All Jobs
 export const getAllJobs = async (req, res) => {
   console.log(req.user)
-  const jobs = await Job.find({})
+  const jobs = await Job.find({ createdBy: req.user.userId })
   res.status(StatusCodes.OK).json({ jobs })
 }
 
 // Create Job
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId
   const job = await Job.create(req.body)
   res.status(StatusCodes.CREATED).json({ job })
 }
