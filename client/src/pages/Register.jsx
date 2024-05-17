64390731b14109743292fcc7407e2d8bc2709b10
thenatural86 <philip.smalls86@gmail.com>
2024-05-17 17:46:48 -0300

    completes register fucntionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import { Form, redirect, useNavigation, Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
 import { FormRow, Logo } from '../components'
+import customFetch from '../utils/customFetch'
 
-export const action = async (data) => {
-  console.log(data)
-  return null
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.post('/auth/register', data)
+    return redirect('/login')
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 }
 
 const Register = () => {
@@ -19,11 +28,11 @@ const Register = () => {
           type='text'
           name='lastName'
           labelText='last name'
-          defaultValue='bob'
+          defaultValue='joe'
         />
         <FormRow type='text' name='location' defaultValue='earth' />
         <FormRow type='email' name='email' defaultValue='joe@joe.com' />
-        <FormRow type='password' name='password' defaultValue='joejoe' />
+        <FormRow type='password' name='password' defaultValue='joejoejoe' />
 
         <button type='submit' className='btn btn-block'>
           submit
