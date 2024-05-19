3e2f24141b70989fa8b76a769c4471cc4898afd1
thenatural86 <philip.smalls86@gmail.com>
2024-05-19 19:07:46 -0300

    add job page

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import { FormRow, FormRowSelect } from '../components'
+import Wrapper from '../assets/wrappers/DashboardFormPage'
+import { useOutletContext } from 'react-router-dom'
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants'
+import { Form, useNavigation, redirect } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import customFetch from '../utils/customFetch'
 
 const AddJob = () => {
-  return <h1>AddJob</h1>
+  const { user } = useOutletContext()
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>add job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' />
+          <FormRow type='text' name='company' />
+          <FormRow type='text' labelText='job location' name='jobLocation' />
+
+          <FormRowSelect
+            labelText='job status'
+            name='jobStatus'
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText='jobType'
+            name='job type'
+            defaultValue={JOB_STATUS.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
+          />
+
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  )
 }
 
 export default AddJob
