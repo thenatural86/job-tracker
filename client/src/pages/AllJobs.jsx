d9386bba9cec7576ca6a3244fc5480b20c3963cf
thenatural86 <philip.smalls86@gmail.com>
2024-05-20 01:16:46 -0300

    adds all jobs structure

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
+import { toast } from 'react-toastify'
+import { JobsContainer, SearchContainer } from '../components'
+import customFetch from '../utils/customFetch'
+import { useLoaderData } from 'react-router-dom'
+import { useContext, createContext } from 'react'
+
+export const loader = async ({ request }) => {
+  try {
+    const { data } = await customFetch.get('/jobs')
+    return { data }
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
+
 const AllJobs = () => {
-  return <h1>AllJobs</h1>
+  const { data } = useLoaderData()
+  return (
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+  )
 }
 
 export default AllJobs
