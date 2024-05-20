b314dcd0585ecfd4bcc13ec315d1b5c7aa39c323
thenatural86 <philip.smalls86@gmail.com>
2024-05-20 18:56:54 -0300

    renders job information

--- client/src/components/JobInfo.jsx
+++ client/src/components/JobInfo.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Wrapper from '../assets/wrappers/JobInfo'
 
-const JobInfo = () => {
-  return <div>JobInfo</div>
+const JobInfo = ({ icon, text }) => {
+  return (
+    <Wrapper>
+      <span className='job-icon'>{icon}</span>
+      <span className='job-text'>{text}</span>
+    </Wrapper>
+  )
 }
 
 export default JobInfo
