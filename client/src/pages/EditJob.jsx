72dcc7acdee3050c798262546c728064105b909f
thenatural86 <philip.smalls86@gmail.com>
2024-05-20 23:20:32 -0300

    adds params to edit page

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,13 +1,19 @@
 import { FormRow, FormRowSelect } from '../components'
 import Wrapper from '../assets/wrappers/DashboardFormPage'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useParams } from 'react-router-dom'
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants'
 import { Form, useNavigation, redirect } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import customFetch from '../utils/customFetch'
 
-export const loader = async () => {
-  return null
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`)
+    return data
+  } catch (error) {
+    toast.error(error.response.data.msg)
+    return redirect('/dashboard/all-jobs')
+  }
 }
 
 export const action = async () => {
@@ -15,6 +21,11 @@ export const action = async () => {
 }
 
 const EditJob = () => {
+  const params = useParams()
+  console.log(params)
+  const { job } = useLoaderData()
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
   return <h1>EditJob</h1>
 }
 
