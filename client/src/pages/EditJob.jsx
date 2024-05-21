4d7b99890723ed4d79cb9baf4541df09f1e28afb
thenatural86 <philip.smalls86@gmail.com>
2024-05-21 09:24:27 -0300

    adds edit job functionality complete

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -37,6 +37,35 @@ const EditJob = () => {
     <Wrapper>
       <Form method='post' className='form'>
         <h4 className='form-title'>edit job</h4>
+        <div className='form-center'>
+          <FormRow type='text' name='position' defaultValue={job.position} />
+          <FormRow type='text' name='company' defaultValue={job.company} />
+          <FormRow
+            type='text'
+            labelText='job location'
+            name='jobLocation'
+            defaultValue={job.jobLocation}
+          />
+          <FormRowSelect
+            name='jobStatus'
+            labelText='job status'
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
       </Form>
     </Wrapper>
   )
