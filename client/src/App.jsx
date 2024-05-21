2615dc8ed8c756757d178e737574cc469118db9d
thenatural86 <philip.smalls86@gmail.com>
2024-05-20 23:14:03 -0300

    adds edit job setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import {
   AllJobs,
   Profile,
   Admin,
+  EditJob,
 } from './pages'
 
 import { action as registerAction } from './pages/Register'
@@ -19,6 +20,8 @@ import { action as loginAction } from './pages/Login'
 import { action as addJobAction } from './pages/AddJob'
 import { loader as dashboardLoader } from './pages/DashboardLayout'
 import { loader as allJobsLoader } from './pages/AllJobs'
+import { loader as editJobLoader } from './pages/EditJob'
+import { action as editJobAction } from './pages/EditJob'
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
@@ -74,6 +77,12 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+          },
+          {
+            path: 'edit-job/:id',
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
