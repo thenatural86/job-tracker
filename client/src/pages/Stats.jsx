f46911fe16ea779e033ea5e382f7cd15865a08a8
thenatural86 <philip.smalls86@gmail.com>
2024-05-25 15:19:51 -0300

    loads stats data

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { ChartsContainer, StatsContainer } from '../components'
+import customFetch from '../utils/customFetch'
+import { useLoaderData } from 'react-router-dom'
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/jobs/stats')
+    return response.data
+  } catch (error) {
+    return error
+  }
+}
 
 const Stats = () => {
-  return <h1>Stats</h1>
+  const { defaultStats, monthlyApplications } = useLoaderData()
+  console.log(defaultStats)
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplications?.length > 0 && (
+        <ChartsContainer data={monthlyApplications} />
+      )}
+    </>
+  )
 }
 
 export default Stats
