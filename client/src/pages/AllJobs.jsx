b8ca974db643c43e0e495aa6376282820c104a30
thenatural86 <philip.smalls86@gmail.com>
2024-05-26 18:46:37 -0300

    adds controlled inputs to search form

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -5,10 +5,19 @@ import { useLoaderData } from 'react-router-dom'
 import { useContext, createContext } from 'react'
 
 export const loader = async ({ request }) => {
-  console.log('hello world')
   try {
-    const { data } = await customFetch.get('/jobs')
-    return { data }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ])
+
+    const { data } = await customFetch.get('/jobs', {
+      params,
+    })
+
+    return {
+      data,
+      searchValues: { ...params },
+    }
   } catch (error) {
     toast.error(error?.response?.data?.msg)
     return error
@@ -18,9 +27,9 @@ export const loader = async ({ request }) => {
 const AllJobsContext = createContext()
 
 const AllJobs = () => {
-  const { data } = useLoaderData()
+  const { data, searchValues } = useLoaderData()
   return (
-    <AllJobsContext.Provider value={{ data }}>
+    <AllJobsContext.Provider value={{ data, searchValues }}>
       <SearchContainer />
       <JobsContainer />
     </AllJobsContext.Provider>
