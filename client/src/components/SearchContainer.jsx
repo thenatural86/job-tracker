eabd6d54b6dce45d065a74ed25e2e0bfbe5bbee4
thenatural86 <philip.smalls86@gmail.com>
2024-05-26 22:07:21 -0300

    adds debounce logic to search

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -8,6 +8,18 @@ const SearchContainer = () => {
   const { searchValues } = useAllJobsContext()
   const { search, jobStatus, jobType, sort } = searchValues
   const submit = useSubmit()
+
+  const debounce = (onChange) => {
+    let timeout
+    return (e) => {
+      const form = e.currentTarget.form
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        onChange(form)
+      }, 2000)
+    }
+  }
+
   return (
     <Wrapper>
       <Form className='form'>
@@ -19,9 +31,9 @@ const SearchContainer = () => {
             type='search'
             name='search'
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form)
-            }}
+            onChange={debounce((from) => {
+              submit(form)
+            })}
           />
           <FormRowSelect
             labelText='job status'
