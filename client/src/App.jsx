8919f4c1e557dffd6c5f86f3b39e1757b77dc320
thenatural86 <philip.smalls86@gmail.com>
2024-05-29 19:22:44 -0300

    error element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ import { action as deleteJobAction } from './pages/DeleteJob'
 import { loader as adminLoader } from './pages/Admin'
 import { action as profileAction } from './pages/Profile'
 import { loader as statsLoader } from './pages/Stats'
+import { ErrorElement } from './components'
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
@@ -79,6 +80,7 @@ const router = createBrowserRouter([
             path: 'stats',
             element: <Stats />,
             loader: statsLoader,
+            errorElement: <ErrorElement />,
           },
           {
             path: 'all-jobs',
