853771c164f2d496ef0f4a4697ec3404a3293b08
thenatural86 <philip.smalls86@gmail.com>
2024-05-29 15:19:56 -0300

    fix update user controller

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,16 +1,28 @@
 import multer from 'multer'
+import DataParser from 'datauri/parser.js'
+import path from 'path'
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // set the directory where uploaded files will be stored
-    cb(null, 'public/uploads')
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname
-    // set the name of the uploaded file
-    cb(null, fileName)
-  },
-})
+const storage = multer.memoryStorage()
 const upload = multer({ storage })
+
+const parser = new DataParser()
+
+export const formatImage = (file) => {
+  console.log(file)
+  const fileExtension = path.extname(file.originalname).toString()
+  return parser.format(fileExtension, file.buffer).content
+}
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // set the directory where uploaded files will be stored
+//     cb(null, 'public/uploads')
+//   },
+//   filename: (req, file, cb) => {
+//     const fileName = file.originalname
+//     // set the name of the uploaded file
+//     cb(null, fileName)
+//   },
+// })
 
 export default upload
